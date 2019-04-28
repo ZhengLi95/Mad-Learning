04cc4a1a2ab969100bdd3f3e207b502b5cd6f973
Skylark <24936522+Skylark0924@users.noreply.github.com>
2019-04-28 09:33:11 +0800

    update

--- Tensorflow_test/3Build_a_NN.py
+++ Tensorflow_test/3Build_a_NN.py
@@ -13,8 +13,9 @@ def add_layer(inputs, inputs_size, outputs_size, activation_function=None):
         outputs = activation_function(outputs_init)
     return outputs
 
+
 # 构建数据
-x_data=np.linspace(-1,1,300,dtype=np.float32)[:,np.newaxis]
+x_data = np.linspace(-1, 1, 300, dtype=np.float32)[:, np.newaxis]
 noise = np.random.normal(0, 0.05, x_data.shape).astype(np.float32)
 y_data = np.square(x_data) - 0.5 + noise
 
@@ -23,18 +24,18 @@ xs = tf.placeholder(tf.float32, [None, 1])
 ys = tf.placeholder(tf.float32, [None, 1])
 
 # 搭建单隐藏层NN，prediction_layer为输出层
-layer1=add_layer(x_data,1,10,activation_function=tf.nn.relu)
-prediction_layer=add_layer(layer1,10,1,activation_function=None)
+layer1 = add_layer(x_data, 1, 10, activation_function=tf.nn.relu)
+prediction_layer = add_layer(layer1, 10, 1, activation_function=None)
 
 # 计算误差损失
-loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
-                     reduction_indices=[1]))
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction_layer),
+                                    reduction_indices=[1]))
 
 # 优化器
 train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 # 必不可少的变量初始化
-init = tf.global_variables_initializer() 
+init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
